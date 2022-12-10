42444f5fce486f00a141aa19971b1175a56885b5
kevinharr <kevinharr@verizon.net>
2022-12-10 16:42:10 -0500

    in middle of check for winning combos

--- js/app.js
+++ js/app.js
@@ -85,8 +85,23 @@ function checkForTie() {
   })
 }
 
+// const winningCombos = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [6, 4, 2],
+// ]
+
+
 function checkForWinner() {
-  
+  for (i = 0; i < 9; i++) {
+    if (board[i]winningCombos[i][]
+  }
+
 }
 function attachingListeners() {
   for (i = 0; i < 9; i++) {
